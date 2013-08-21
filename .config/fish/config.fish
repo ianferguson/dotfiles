d9c8d071d11913c3763ae2a59627f7c47371a4db
ian <iferguson@catalist.us>
2013-08-21 11:13:09 -0400

    add gitnuke function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,1 @@
-set -x PATH /opt/local/lib/postgresql92/bin/ /opt/local/bin /opt/local/sbin $PATH $HOME/bin
+set -gx PATH /Users/iferguson/bin/apache-maven-2.2.1/bin/ /opt/local/lib/postgresql92/bin/ $PATH
