da47bc39808fe2539f115d48471cf800ea8667e6
ian <iferguson@ondeck.com>
2013-09-12 14:57:09 -0400

    don't execute stuff directly accessed via http

--- master-brewer.sh
+++ master-brewer.sh
@@ -2,11 +2,19 @@
 
 set -e
 
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+ruby -e brew-starter.rb 
+read -p "Install complete, press enter to continue..."
+
+brew doctor
+read -p "Doctor check complete, press enter to continue..."
 
 brew update
+read -p "Update complete, press enter to run install of default brews..."
 
 for BREW in $(cat brews); do
+    echo "installing $BREW!"
     brew install $BREW
 done
+
+echo "you're done brewing, kick back and enjoy the 6 pack, bro!"
 
