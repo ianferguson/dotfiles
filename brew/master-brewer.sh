405c2e1001f2552f4b8695cc54bbd3e547035cf5
ian <ian@tremblingfire.com>
2014-08-01 17:21:57 -0400

    force upgrades after cask tapping

--- brew/master-brewer.sh
+++ brew/master-brewer.sh
@@ -13,6 +13,7 @@ brew update
 
 echo "tapping caskroom"
 brew tap phinze/cask
+brew upgrade
 
 echo "installing brews"
 for BREW in $(cat brews); do
@@ -25,6 +26,8 @@ for CASK in $(cat casks); do
     echo "installing $CASK"
     brew cask install $CASK --force
 done
+
+brew upgrade
 
 echo "cleaning up old versions"
 brew cleanup
