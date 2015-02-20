b1a8adaeaa08b74067615a67efb4152b6a40efe2
ian ferguson <ian@labmarie.com>
2015-02-19 20:48:08 -0500

    auto set, or reset, GOROOT after updates

--- fish/.config/fish/functions/freshie.fish
+++ fish/.config/fish/functions/freshie.fish
@@ -1,6 +1,13 @@
 function freshie
 	brew update
-	and brew upgrade $argv
-    and brew cask cleanup
-    and brew cleanup
+  and brew upgrade $argv
+  and brew cask cleanup
+  and brew cleanup
+
+  # in the case that go is updated, the fish env GOROOT will be pointing at
+  # the old GOROOT value, which is set mostly for the benefit of godef, godoc
+  # and a few other tools that whine if its not set in the shell env, so
+  # go ahead and ask the go on our path (which will be the homebrew managed one)
+  # what its root is and then export that universally to our fish shells
+  set -Ux GOROOT (go env GOROOT)
 end
