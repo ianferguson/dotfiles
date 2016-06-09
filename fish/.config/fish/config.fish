667d6b9217c9e22de4b2d3a2e3e8393518aec2a2
Ian Ferguson <ian@ianferguson.io>
2016-06-09 11:22:26 -0400

    do not source local opower env file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,5 @@
 #!/usr/local/bin/fish
 set -gx PATH /usr/local/bin /usr/local/sbin $PATH ~/bin $GOPATH/bin
 
-# load in opower specific env vars
-source ~/.opower_env.fish
-
 # do whatever horrible things rbenv needs to do to make ruby not as horrible
 status --is-interactive; and . (rbenv init -|psub)
