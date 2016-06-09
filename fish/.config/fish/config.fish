d07973f1fb01139d56b5978c2c2da31c26c19528
Ian Ferguson <ian@ianferguson.io>
2016-06-09 11:25:57 -0400

    install fisherman rbenv plugin

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,2 @@
 #!/usr/local/bin/fish
 set -gx PATH /usr/local/bin /usr/local/sbin $PATH ~/bin $GOPATH/bin
-
-# do whatever horrible things rbenv needs to do to make ruby not as horrible
-status --is-interactive; and . (rbenv init -|psub)
