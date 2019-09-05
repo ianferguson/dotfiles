d2376086e5ce2e01dbe6e928ae69f2adcb4a4a27
Ian Ferguson <ian.ferguson@datadoghq.com>
2019-09-05 12:55:01 -0400

    tweak vault-vault and vault-login

--- fish/.config/fish/functions/vault-login.fish
+++ fish/.config/fish/functions/vault-login.fish
@@ -1,6 +1,8 @@
 function vault-login
   kubectl login $argv[1] --vault-only
   bass eval (kubectl vault print-auth)
+  echo $VAULT_TOKEN
   echo $VAULT_TOKEN | envchain --set $VAULT_ADDR VAULT_TOKEN
+  echo $VAULT_TOKEN | ddtool-vault-token-helper store
 end
 
--- fish/.config/fish/functions/vault-vault.fish
+++ fish/.config/fish/functions/vault-vault.fish
@@ -1,4 +1,6 @@
 function vault-vault
+  set -q VAULT_ADDR; or echo "VAULT_ADDR must be set"
+  echo $VAULT_ADDR
   envchain $VAULT_ADDR vault $argv
 end
 
