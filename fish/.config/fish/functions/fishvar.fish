49234a5df5a0c6b26575f622973b750898cf0de9
Ian Ferguson <ian@ianferguson.io>
2019-09-12 12:46:30 -0400

    Remove fishvar in favor of bass

--- fish/.config/fish/functions/fishvar.fish
+++
@@ -1,4 +0,0 @@
-function fishvar -d 'Mostly translate Bash var sets to fishy forms you can eval (echo $bash) on'
-  eval ($argv) | sed -e 's/^/set /' -e 's/export/-x/' -e 's/=/ /g' | tr '\n' ';'
-end
-
