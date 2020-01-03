5a64c64f5db89f9dd67de7d337c73f63b9b4d4f9
Ian Ferguson <ian.ferguson@datadoghq.com>
2020-01-03 15:17:07 -0500

    refine how fish path is configured and maintained

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,1 @@
 #!/usr/local/bin/fish
-set -gx PATH /usr/local/bin /usr/local/sbin $PATH ~/bin $GOPATH/bin
-set -gx PATH '/Users/ian.ferguson/.rbenv/shims' $PATH
-set -gx PATH $PATH ~/dd/devtools/bin
-set -gx RBENV_SHELL fish
